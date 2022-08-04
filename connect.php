809357597b8122a08930b252349d0d9b9431ad1b
LuNguyenQuocHuy <90846265+quochuy0205@users.noreply.github.com>
2022-08-04 13:07:46 +0700

    Update connect.php

--- connect.php
+++ connect.php
@@ -2,7 +2,7 @@
 $servername="localhost";
 $username="root";
 $password="";
-$dbname="dungcuthethao";
+$dbname="webdattourdulich";
 
 $conn = new mysqli($servername, $username, $password, $dbname);
 
